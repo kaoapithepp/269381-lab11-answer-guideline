782ca055760f1298051a9d3611783b5d05fa6d2c
kaoapithepp <momogg334@gmail.com>
2022-09-21 21:46:58 +0700

    fix: edited column display name in problem06

--- S06_Lab11_UPDATE.sql
+++ S06_Lab11_UPDATE.sql
@@ -2,6 +2,6 @@ UPDATE ArtistCompany
 SET CompanyName = 'HYBE Entertainment' , CompanyNo = 1033
 WHERE CompanyNo = 1003;
 
-SELECT AC.CompanyNo CompanyNo, AC.CompanyName CompanyName, AG.ArtistGrpName ArtistGrpName
+SELECT AC.CompanyNo CompanyNo, AC.CompanyName CompanyName, AG.GroupName GroupName
 FROM ArtistCompany AC
 	JOIN ArtistGroup AG ON AC.CompanyNo = AG.CompanyNum;
